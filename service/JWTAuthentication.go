2c0c66a7aa87fa0051b9a58546c93fc49a6d90a7
matthewbennett <tedja2727@gmail.com>
2021-10-10 00:19:06 +0700

    add Login credentials login functions

--- service/JWTAuthentication.go
+++ service/JWTAuthentication.go
@@ -1,1 +1,22 @@
 package service
+
+
+type LoginService interface {
+	LoginUser(email string, password string)bool
+}
+
+type LoginInformation struct {
+	email string
+	password string
+}
+
+func (info *LoginInformation) LoginUser(email string, password string) bool {
+	return info.email == email && info.password == password
+}
+
+func StaticLoginService() LoginService {
+	return &LoginInformation{
+		email: "matthewbennett.mail@gmail.com",
+		password:  "Pudge1001!!",
+	}
+}
