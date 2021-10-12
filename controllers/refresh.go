f1f0d15f3120cac1ae417e4942722d6abc4f74cd
matthewbennett <tedja2727@gmail.com>
2021-10-12 13:26:17 +0700

    create new refresh file and modify category controller

---
+++ controllers/refresh.go
@@ -0,0 +1,9 @@
+package controllers
+
+//func Refresh(c *gin.Context) {
+//	mapToken := map[string]string{}
+//	if err := c.ShouldBindJSON(&mapToken); err != {
+//		c.JSON(http.StatusUnprocessableEntity, err.Error())
+//		return
+//	}
+//}
